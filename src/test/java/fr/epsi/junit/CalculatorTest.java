8b08fce277cf061be34446b9a24f4d06a78d2f6d
Vubes <55530734+Vubes@users.noreply.github.com>
2021-06-23 11:10:04 +0200

    Add - substract - multi - divide
    Add the operations

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -12,12 +12,34 @@ public class CalculatorTest {
     }
 
     public void should_add_two_numbers(){
-       // TODO
+       // TO TEST
+        int a = 5;
+        int b = 6;
+        int result = a + b;
+        Assertions.assertEquals(result, this.calculator.add(a, b));
     }
 
     public void should_substract_two_numbers(){
-        // TODO
+        // TO TEST
+        int a = 16;
+        int b = 8;
+        int result = a - b;
+        Assertions.assertEquals(result, this.calculator.substract(a, b));
     }
 
+    public void should_multi_two_numbers(){
+        // TO TEST
+        int a = 10;
+        int b = 2;
+        int result = a*b;
+        Assertions.assertEquals(result, this.calculator.multi(a, b));
+    }
 
+    public void should_divide_two_numbers(){
+        // TO TEST
+        int a = 40;
+        int b = 10;
+        int result = a/b;
+        Assertions.assertEquals(result, this.calculator.div(a, b));
+    }
 }
